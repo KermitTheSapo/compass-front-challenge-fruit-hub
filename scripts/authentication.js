8a836ead7cdd32040360173d4701ad46ba7ae328
Robert <rjsf06@gmail.com>
2022-09-02 18:46:44 -0300

    :sparkles: feat: starting authentication functionality

---
+++ scripts/authentication.js
@@ -0,0 +1,15 @@
+// const input = document.querySelector(".writeName")
+// const button = document.querySelector(".continue")
+// const text = document.querySelector(".welcome")
+
+// button.addEventListener("click", () => {
+//     // if(!input.value) {
+//     //     alert("aaa")
+//     //     return;
+//     // }
+//     const user = localStorage.setItem("userName", input.value)    
+// })
+
+// text.innerHTML = "aaaaaaaa"
+
+// // Welcome, Chris
---
+++ scripts/home.js
@@ -0,0 +1,8 @@
+const next = document.querySelector(".next")
+const back = document.querySelector(".next")
+
+
+next.checked = true
+
+
+// 
